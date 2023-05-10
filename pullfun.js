4d0c37073f590aa15b5241ecf3fa23aaf3c08085
markrexon <markrexon1999@gmail.com>
2023-05-10 17:46:40 +0530

    sec

--- pullfun.js
+++ pullfun.js
@@ -1,11 +1,11 @@
 const _ =require("lodash");
 
 var array = [1, 2, 3, 4];
-// var evens = _.remove(array, (n)=>  n % 2 == 0);
-// console.log(evens)
-// console.log(_.reverse(array))
-// console.log(_.slice(array,0,3))
-// console.log(_.sortedIndex())
+var evens = _.remove(array, (n)=>  n % 2 == 0);
+console.log(evens)
+console.log(_.reverse(array))
+console.log(_.slice(array,0,3))
+console.log(_.sortedIndex())
 
 var users = [
     { 'user': 'barney',  'active': true },
