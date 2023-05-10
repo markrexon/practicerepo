66d87584353e243c2ed613f08aa18c4dc3cba1f2
markrexon <markrexon1999@gmail.com>
2023-05-10 17:45:00 +0530

    second

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ var users = [
   { 'user': 'fred',    'active': false },
   { 'user': 'pebbles', 'active': false }
 ];
-// console.log(_.dropRightWhile(users, (user)=> { return !user.active; }));
-// console.log(_.findLastIndex(users,(user)=>user.user=="pebbles"))
+console.log(_.dropRightWhile(users, (user)=> { return !user.active; }));
+console.log(_.findLastIndex(users,(user)=>user.user=="pebbles"))
 var array = [1, 2, 3,1,3,2];
-// console.log(_.fill(array,8,0,2))
+console.log(_.fill(array,8,0,2))
 
 const obj=[ 
     [2, 1], [3, 2,10],
     ['c',3]];
-  // console.log(_.fromPairs(obj))
+  console.log(_.fromPairs(obj))
 
-  // console.log(_.indexOf(array,2))
+  console.log(_.indexOf(array,2))
 
-  // console.log(_.join(array,''))
-  // console.log(_.last(array))
+  console.log(_.join(array,''))
+  console.log(_.last(array))
  _.pullAll(array,1,2);
  console.log(array)
-  // for(const i in array)
-  //   console.log(_.nth(array, i))
+  for(const i in array)
+    console.log(_.nth(array, i))
  
